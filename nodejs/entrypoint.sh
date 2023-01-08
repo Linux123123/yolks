7bc647f56b9cca6fe3740144bcf0993e13f394e3
Linux123123 <me@linux123123.com>
2023-01-08 12:36:53 +0200

    Update and add pteranodon image

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
+cd /home/container || exit
 
-TZ=${TZ:-UTC}
-export TZ
-
-INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-cd /home/container || exit 1
-
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mnode -v\n"
+# Print Node.js Version
 node -v
 
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
-# shellcheck disable=SC2086
-exec env ${PARSED}
+# Run the Server
+eval "${MODIFIED_STARTUP}"
