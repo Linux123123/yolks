850493ed2298d634c9abb40e0efa2c3e42ddddc7
Linux123123 <me@linux123123.com>
2026-08-06 14:35:10 +0300

    feat: dotnet

---
+++ dotnet/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /home/container || exit
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Print .NET version.
+dotnet --version
+
+# Replace startup variables.
+MODIFIED_STARTUP=$(echo -e "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run server.
+eval "${MODIFIED_STARTUP}"
